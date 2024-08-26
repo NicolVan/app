9913544f9cbb013bac6e40a27379ba9f82a9a500
Nikola Vaniščáková <161007729+NicolVan@users.noreply.github.com>
2024-08-26 12:06:21 +0200

    Delete server directory

--- server/models/SaveRecipe.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const savedRecipeSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  recipeId: { type: Schema.Types.ObjectId, ref: 'Recipe', required: true }
-});
-
-module.exports = mongoose.model('SavedRecipe', savedRecipeSchema);
